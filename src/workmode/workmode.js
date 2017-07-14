21a9900bf420d1df3dda279fbbdd8fbad86885e4
Helga de Ville <szklarska@hpe.com>
2017-07-14 16:38:46 +0200

    More work

--- src/workmode/workmode.js
+++ src/workmode/workmode.js
@@ -42,7 +42,7 @@ export class WorkMode {
             for(var i = 0 ; i < me.radios.length ; i++) {
                 devices.push({
                     name: me.radios[i].ifname,
-                    type: 'radio device',
+                    type: 'radio',
                     description: 'TBD',
                     src: me.radios[i]
                 });
@@ -65,6 +65,129 @@ export class WorkMode {
             }
         }
         return true;
+    }
+    
+    getParameters(line) {
+        var parameters = [ { 
+                name: 'MAC:',
+                single: true,
+                begin: true
+            }, {
+                name: 'ESSID:',
+                single: true
+            }, { 
+                name:'Mode:'
+            }, {
+                name: 'Channel:'
+            }, {
+                name: 'Signal:'
+            }, {
+                name: 'Quality:'
+            }, {
+                name: 'Encryption:',
+                single: true
+            }
+        ];
+        var obj = {};
+        var prm = {};
+        for(var i = 0 ; i < parameters.length ; i++) {
+            var param = parameters[i];
+            var idx = line.indexOf(param.name);
+            if (idx >= 0) {
+                if (param.single) {
+                    
+                } else {
+                    
+                }
+            }
+        }
+    }
+    
+    scan() {
+        let data = {
+        };
+        this.overlay.open();
+        this.FEC.submit('cgi-bin/scan.text', data)
+            .then(response => {
+                this.overlay.close();
+                // response.response contains text
+                var aps = [];
+                var lines = response.response.split('\n');
+                var current = null;
+                for(var i = 0 ; i < lines.length ; i++) {
+                    var line = lines[i];
+                    // MAC: ESSID: Mode: Channel: Signal: Quality: Encryption:
+                    var macIndex = line.indexOf('MAC:');
+                    var essidIndex = line.indexOf('ESSID:');
+                    var modeIndex = line.indexOf('Mode:');
+                    var channelIndex = line.indexOf('Channel:');
+                    var signalIndex = line.indexOf('Signal:');
+                    var qualityIndex = line.indexOf('Quality:');
+                    var encryptionIndex = line.indexOf('Encryption:');
+                    if (macIndex >= 0) {
+                        // found mac
+                        if (current) {
+                            aps.push(current);
+                        }
+                        current = {
+                            mac: line.substring(macIndex + 5).trim()
+                        };
+                    }
+                    if (essidIndex >= 0) {
+                        current.ssid = line.substring(essidIndex + 7).trim();
+                        if (current.ssid.startsWith('"') && current.ssid.endsWith('"')) {
+                            current.ssid = current.ssid.substring(1, current.ssid.length - 1);
+                        }
+                    }
+                    // both mode and channel
+                    if (modeIndex >= 0 && channelIndex >= 0) {
+                        if (channelIndex > modeIndex) {
+                            current.mode = line.substring(modeIndex + 6, channelIndex - 1).trim();
+                            current.channel = line.substring(channelIndex + 9).trim();
+                        } else {
+                            current.channel = line.substring(channelIndex + 9, modeIndex - 1).trim();
+                            current.mode = line.substring(modeIndex + 6).trim();
+                        }
+                    } else {
+                        if (modeIndex >= 0) {
+                            current.mode = line.substring(modeIndex + 6).trim();
+                        }
+                        if (channelIndex >= 0) {
+                            current.channel = line.substring(channelIndex + 9).trim();
+                        }
+                    }
+                    // both signal and quality
+                    if (signalIndex >= 0 && qualityIndex >= 0) {
+                        if (qualityIndex > signalIndex) {
+                            current.signal = line.substring(signalIndex + 6, qualityIndex - 1).trim();
+                            current.quality = line.substring(qualityIndex + 9).trim();
+                        } else {
+                            current.quality = line.substring(qualityIndex + 9, signalIndex - 1).trim();
+                            current.signal = line.substring(signalIndex + 6).trim();
+                        }
+                    } else {
+                        if (modeIndex >= 0) {
+                            current.signal = line.substring(signalIndex + 6).trim();
+                        }
+                        if (channelIndex >= 0) {
+                            current.quality = line.substring(qualityIndex + 9).trim();
+                        }
+                    }
+                    // encryption
+                    if (encryptionIndex >= 0) {
+                        current.encryption = line.substring(encryptionIndex + 12).trim();
+                    }
+                }
+                if (current) {
+                    aps.push(current);
+                }
+                // TODO
+                console.log(aps);
+            }).catch(error => {
+                this.overlay.close();
+                console.log('Error scanning AP networks');
+                this.dialogService.error('Ooops ! Error occured:\n' + error.statusCode + '/' + error.statusText + '\n' + error.response);
+            });
     }
     
     submit() {
